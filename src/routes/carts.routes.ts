d6ec122579c4fd3de1ea9ef04669827adbfd9852
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-05 12:46:07 -0500

    add format to the file

--- src/routes/carts.routes.ts
+++ src/routes/carts.routes.ts
@@ -1,17 +1,17 @@
 import { request, response, Router } from "express";
 
 class CartsRoutes {
-    public router: Router
-    constructor(){
-        this.router = Router()
-        this.routes()
-    }
-    routes(){
-        this.router.get('/carts', (req, res)=>{
-            res.send('<h1>HEllo from carts route!!</h1>')
-        })
-    }
+  public router: Router;
+  constructor() {
+    this.router = Router();
+    this.routes();
+  }
+  routes() {
+    this.router.get("/carts", (req, res) => {
+      res.send("<h1>HEllo from carts route!!</h1>");
+    });
+  }
 }
 
-const cartsRoutes =  new CartsRoutes()
-export default cartsRoutes.router
+const cartsRoutes = new CartsRoutes();
+export default cartsRoutes.router;
