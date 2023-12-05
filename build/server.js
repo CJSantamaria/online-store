7a545bfe702aa74b0f4582fd917afdf1dd72bc11
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-05 01:03:03 -0500

    add script for waching changes in ts files

--- build/server.js
+++ build/server.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
@@ -11,6 +12,7 @@ class Server {
     }
     config() {
         this.app.set('port', process.env.PORT || 3000);
+        this.app.use((0, morgan_1.default)('dev'));
     }
     routes() {
     }
