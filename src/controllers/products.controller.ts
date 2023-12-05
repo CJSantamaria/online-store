e1030dc15bf405523ddabc3faa902a12860eff8f
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-05 12:25:29 -0500

    add basic serialization calls to the controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express";
+import fs from "fs";
+import { dataPath } from "../config/config";
 
-export class ProductsController{
-    public constructor(){
-    }
-    public getProducts(req: Request, res: Response): void{
-        res.send('<h1>Hello from get products in the controller!! </h1>')
-    }
-    public getProduct(req: Request, res:Response): void{
-        res.send('<h1>Hello from get singular product in the controller</h1>')
-    }
+export class ProductsController {
+  public constructor() {}
+  public async getProducts(req: Request, res: Response): Promise<void> {
+    await fs.readFile(dataPath.products, "utf-8", (error, data) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send("<h1>An error has ocurred</h1>");
+      } else {
+        res.send(data);
+      }
+    });
+  }
+  public getProduct(req: Request, res: Response): void {
+    res.send("<h1>Hello from get singular product in the controller</h1>");
+  }
 }
 
-const productsController = new ProductsController()
-export { productsController }
-
+const productsController = new ProductsController();
+export { productsController };
