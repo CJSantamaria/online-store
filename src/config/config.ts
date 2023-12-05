533e1b5de425c74a2a47e6440e43be01f7fdba07
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-05 11:23:03 -0500

    create the configurations file

---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+const root = process.cwd()
+const dataBaseDir = '/database'
+export const dataPath = {
+    products : root + dataBaseDir + '/products',
+    carts : root + dataBaseDir + '/carts'
+}
