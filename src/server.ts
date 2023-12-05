cb99eb7c89b216233422a3a267d8c31f4a5e21c3
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-05 12:12:38 -0500

    use the new configs in the main file

--- src/server.ts
+++ src/server.ts
@@ -3,27 +3,27 @@ import morgan from "morgan";
 import productsRoutes from "./routes/products.routes";
 import cartsRoutes from "./routes/carts.routes";
 
-class Server{
-    public app: express.Application
-    constructor(){
-        this.app = express()
-        this.config()
-        this.routes()
-    }
-    
-    config(){
-        this.app.set('port', process.env.PORT || 8080)
-        this.app.use(morgan('dev'))
-    }
-    routes(){
-        this.app.use('/api/products',productsRoutes)
-        this.app.use('/api',cartsRoutes)
-    }
-    start(){
-        this.app.listen(this.app.get('port'), ()=>{
-            console.log(`Server listening on port ${this.app.get('port')}..`)
-        })
-    }
+class Server {
+  public app: express.Application;
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+
+  config() {
+    this.app.set("port", process.env.PORT || 8080);
+    this.app.use(morgan("dev"));
+  }
+  routes() {
+    this.app.use("/api/products", productsRoutes);
+    this.app.use("/api", cartsRoutes);
+  }
+  start() {
+    this.app.listen(this.app.get("port"), () => {
+      console.log(`Server listening on port ${this.app.get("port")}..`);
+    });
+  }
 }
-const server = new Server()
-server.start()
+const server = new Server();
+server.start();
