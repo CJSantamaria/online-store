e1633de0843a78b97a3ed3ace541fd3b90ddb999
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-05 12:09:04 -0500

    add the configurarion file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
-const root = process.cwd()
-const dataBaseDir = '/database'
+import path from "path";
+const root = process.cwd();
+const dataBaseDir = "/database";
 export const dataPath = {
-    products : root + dataBaseDir + '/products',
-    carts : root + dataBaseDir + '/carts'
-}
+  products: path.join(root, dataBaseDir, "/products"),
+  carts: path.join(root, dataBaseDir, "/carts")
+};
