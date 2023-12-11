cad3f69321665a5ec055cd769d3a405b8bedeb05
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-11 16:32:24 -0500

    add functionality for getting products from the datafile

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,24 +1,52 @@
 import { Request, Response } from "express";
-import fs from "fs";
-import { dataPath } from "../config/config";
+import { v4 as uuidv4 } from "uuid";
+import Product from "../types/product";
+import { dataFromFile } from "../helpers/data.helper";
 
 export class ProductsController {
   public constructor() {}
-  public async getProducts(req: Request, res: Response): Promise<void> {
-    await fs.readFile(dataPath.products, "utf-8", (error, data) => {
-      if (error) {
-        console.log(error);
-        return res.status(500).send("<h1>An error has ocurred</h1>");
-      } else {
-        res.send(data);
-      }
-    });
+
+  public getProducts(req: Request, res: Response) {
+    const products = dataFromFile.products;
+    try {
+      console.log(products);
+      res.send(products);
+    } catch (error) {
+      console.log(error);
+      return res.json({ msg: "An error has ocurred" });
+    }
   }
   public async getProduct(req: Request, res: Response): Promise<void> {
     res.send("<h1>Hello from get singular product in the controller</h1>");
   }
-  public async postProducts(req: Request, res: Response): Promise<void> {
-    
+  public async createProduct(req: Request, res: Response): Promise<void> {
+    const products = dataFromFile.products;
+    const id = uuidv4();
+    const {
+      title,
+      description,
+      code,
+      price,
+      status,
+      stock,
+      category,
+      thumbnails,
+    } = req.body;
+    const product: Product = {
+      id,
+      title,
+      description,
+      code,
+      price,
+      status,
+      stock,
+      category,
+      thumbnails,
+    };
+    products.push(product);
+    console.log(products);
+
+    res.json({ msg: "Product successfully created" });
   }
 }
 
