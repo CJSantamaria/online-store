68dc3af77ad57d71fd50d4805297b79c3ea51621
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-11 16:36:08 -0500

    add the product type

---
+++ src/types/product.d.ts
@@ -0,0 +1,11 @@
+export default interface Product {
+    id: string;
+    title: string;
+    description: string;
+    code: string;
+    price: number;
+    status: boolean;
+    stock: number;
+    category: string;
+    thumbnails: string[];
+  }
