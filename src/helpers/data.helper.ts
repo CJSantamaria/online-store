1a3f832a1eefb9004bd60de193bc4cf48457d9b5
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-15 09:57:03 -0500

    tweak the readFile function to reduce the amount of code lines

--- src/helpers/data.helper.ts
+++ src/helpers/data.helper.ts
@@ -3,27 +3,48 @@ import Product from "../types/product";
 import Cart from "../types/cart";
 import { dataPath } from "../config/config";
 
-class DataFromFile {
-  public products: Product[] = [];
-  public carts: Cart[] = [];
-  constructor() {
-    fs.readFile(dataPath.products, "utf-8", (error, data) => {
+class DataFile {
+  private products: Product[] = [];
+  private carts: Cart[] = [];
+
+  private Paths: { [key: string]: string } = {
+    products: dataPath.products,
+    carts: dataPath.carts,
+  };
+
+  private readFile(entity: string) {
+    fs.readFile(this.Paths[entity], "utf-8", (error, data) => {
       if (error) {
         console.log(error.message);
         return { message: error.message };
       } else {
-        this.products = JSON.parse(data);
-      }
-    });
-    fs.readFile(dataPath.carts, "utf-8", (error, data) => {
-      if (error) {
-        console.log(error.message);
-        return { message: error.message };
-      } else {
-        this.carts = JSON.parse(data);
+        switch (entity) {
+          case "products":
+            this.products = JSON.parse(data);
+            break;
+          case "carts":
+            this.carts = JSON.parse(data);
+            break;
+          default:
+            throw new Error(`No such entity: ${entity}`);
+        }
       }
     });
   }
+
+  public readProducts(): Product[] {
+    this.readFile("products");
+    return this.products;
+  }
+
+  public readCarts(): Cart[] {
+    this.readFile("carts");
+    return this.carts;
+  }
+
+  public saveProducts() {
+    console.log("Hello from the datafile class!!!");
+  }
 }
 
-export const dataFromFile = new DataFromFile();
+export const dataFile = new DataFile();
