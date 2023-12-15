cda6df3f31cf614904c1f61eee608776a4b9be31
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-15 09:54:56 -0500

    add get one product functionality

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,26 +1,45 @@
 import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 import Product from "../types/product";
-import { dataFromFile } from "../helpers/data.helper";
+import { dataFile } from "../helpers/data.helper";
 
 export class ProductsController {
   public constructor() {}
 
-  public getProducts(req: Request, res: Response) {
-    const products = dataFromFile.products;
+  // get the whole list of products
+
+  public getProducts(req: Request, res: Response): Response {
     try {
-      console.log(products);
+      const products : Product[] = dataFile.readProducts();
       res.send(products);
+      return res.status(200)
     } catch (error) {
       console.log(error);
       return res.json({ msg: "An error has ocurred" });
     }
   }
-  public async getProduct(req: Request, res: Response): Promise<void> {
-    res.send("<h1>Hello from get singular product in the controller</h1>");
+
+  // get a single product by using its ID
+
+  public getProduct(req: Request, res: Response): Response {
+    try {
+      const products : Product[] = dataFile.readProducts();
+      const productId = req.params.id
+      const product = products.find(p => p.id ===productId)
+      if (product){
+        return res.status(200).send(product);
+      } else {
+        return res.status(404).json({msg:"No product matches that ID"})
+      }
+    } catch (error) {
+      console.log(error);
+      return res.json({ msg: "An error has ocurred" });
+    }
   }
+
+  // create a new product
+
   public async createProduct(req: Request, res: Response): Promise<void> {
-    const products = dataFromFile.products;
     const id = uuidv4();
     const {
       title,
@@ -43,6 +62,7 @@ export class ProductsController {
       category,
       thumbnails,
     };
+    const products = dataFile.readProducts();
     products.push(product);
     console.log(products);
 
