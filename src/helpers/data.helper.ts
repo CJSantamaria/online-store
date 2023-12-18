4dc55dc0dc2fc8b7c67795275d31c3553e7fa0da
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-18 11:25:47 -0500

    change fs for fs/promises to simplify the functions code and solve an async related bug when calling fs.readFile fucntion

--- src/helpers/data.helper.ts
+++ src/helpers/data.helper.ts
@@ -1,58 +1,48 @@
-import fs, { readFile } from "fs";
+import fs from "fs/promises";
 import Product from "../types/product";
 import Cart from "../types/cart";
 import { dataPath } from "../config/config";
 
 class DataFile {
-  private products: Product[] = [];
-  private carts: Cart[] = [];
+  // private products: Product[] = [];
+  // private carts: Cart[] = [];
 
-  private Paths: { [key: string]: string } = {
-    products: dataPath.products,
-    carts: dataPath.carts,
-  };
-
-  private readFile(entity: string) {
-    fs.readFile(this.Paths[entity], "utf-8", (error, data) => {
-      if (error) {
-        return { message: error.message };
-      } else {
-        switch (entity) {
-          case "products":
-            this.products = JSON.parse(data);
-            break;
-          case "carts":
-            this.carts = JSON.parse(data);
-            break;
-          default:
-            throw new Error(`No such entity: ${entity}`);
-        }
-      }
-    });
+  public async readProductsFile(): Promise<Product[]> {
+    try {
+      const rawData = await fs.readFile(dataPath.products, "utf-8");
+      return JSON.parse(rawData);
+    } catch (err) {
+      throw new Error(`Error reading products file: ${err}`);
+    }
   }
 
-  private writeFile(entity: string, products: string) {
-    fs.writeFile(this.Paths[entity], products, "utf-8", (error) => {
-      if (error) {
-        return { message: error.message };
-      }
-    });
+  public async writeProductsFile(products: Product[]): Promise<void> {
+    try {
+      const dataToWrite = JSON.stringify(products, null, 2);
+      await fs.writeFile(dataPath.products, dataToWrite, "utf-8");
+    } catch (error) {
+      throw new Error(`Error writing products file: ${error}`);
+    }
   }
 
-  public readProducts(): Product[] {
-    this.readFile("products");
-    return this.products;
+  public async readCartsFile(): Promise<Cart[]> {
+    try {
+      const rawData = await fs.readFile(dataPath.carts, "utf-8");
+      return JSON.parse(rawData);
+    } catch (error) {
+      throw new Error(`Error reading carts file: ${error}`);
+    }
   }
 
-  public readCarts(): Cart[] {
-    this.readFile("carts");
-    return this.carts;
+  public async writeCartsFile(carts: Cart[]): Promise<void> {
+    try {
+      const dataToWrite = JSON.stringify(carts, null, 2);
+      await fs.writeFile(dataPath.carts, dataToWrite, "utf-8");
+    } catch (error) {
+      throw new Error(`Error writing carts file: ${error}`);
+    }
   }
 
-  public saveProducts(products: Product[]) {
-    const stringProducts = JSON.stringify(products, null, 2);
-    this.writeFile("products", stringProducts);
-  }
 }
 
 export const dataFile = new DataFile();
