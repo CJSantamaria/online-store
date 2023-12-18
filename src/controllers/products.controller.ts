9bba2289322f83f782aeef9902dec5b2b3c3313f
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-18 18:29:41 -0500

    add product update functionality

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -2,18 +2,20 @@ import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 import Product from "../types/product";
 import { dataFile } from "../helpers/data.helper";
-import { CLIENT_RENEG_LIMIT } from "tls";
 
 export class ProductsController {
   public constructor() {}
 
-  // get the whole list of products
+  // get a specific page of the list of products
 
   public async getProducts(req: Request, res: Response): Promise<Response> {
-    const page: number = req.body.page ? <number><unknown>req.body.page : 1;
-    const limit: number = req.body.limit ? <number><unknown>req.body.limit : 2;
+    const page: number = req.body.page ? <number>(<unknown>req.body.page) : 1;
+    const limit: number = req.body.limit
+      ? <number>(<unknown>req.body.limit)
+      : 2;
     const startIndex: number = (page - 1) * limit;
     const endIndex: number = page * limit;
+
     try {
       const allProducts: Product[] = await dataFile.readProductsFile();
       const products = allProducts.slice(startIndex, endIndex);
@@ -27,8 +29,8 @@ export class ProductsController {
 
   public async getProduct(req: Request, res: Response): Promise<Response> {
     try {
-      const products: Product[] = await dataFile.readProductsFile();
-      const product = products.find((p) => p.id === req.params.id);
+      const products = await dataFile.readProductsFile();
+      const product = products.find((p) => p.id === req.params.pid);
       if (product) {
         return res.status(200).send(product);
       } else {
@@ -69,6 +71,31 @@ export class ProductsController {
     products.push(product);
     dataFile.writeProductsFile(products);
     res.json({ msg: "Product successfully created" });
+  }
+
+  // update a product
+
+  public async updateProduct(req: Request, res: Response): Promise<Response> {
+    const products = await dataFile.readProductsFile();
+    const index = products.findIndex((p) => p.id === req.params.pid);
+    console.log("index:", index)
+    if (index < 0) {
+      return res.status(404).json({ msg: "No product matches that ID" });
+    } else {
+
+      if (req.body.title) products[index].title = req.body.title;
+      if (req.body.description)
+        products[index].description = req.body.description;
+      if (req.body.code) products[index].code = req.body.code;
+      if (req.body.price) products[index].price = req.body.price;
+      if (req.body.status) products[index].status = req.body.status;
+      if (req.body.stock) products[index].stock = req.body.stock;
+      if (req.body.category) products[index].category = req.body.category;
+      if (req.body.thumbnails) products[index].thumbnails = req.body.thumbnails;
+
+      dataFile.writeProductsFile(products);
+      return res.status(200).json({ msg: "Product successfully updated" });
+    }
   }
 }
 
