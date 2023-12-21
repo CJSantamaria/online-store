5c78d9018575e8c698cd8a334749a3d69e9e30bf
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-21 12:48:31 -0500

    add error management to the controller functions

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -21,7 +21,7 @@ class ProductsController {
       const products = allProducts.slice(startIndex, endIndex);
       return res.status(200).send(products);
     } catch (error) {
-      return res.json({ msg: "An error has ocurred" });
+      return res.status(500).json({ msg: error.message });
     }
   }
 
@@ -38,24 +38,27 @@ class ProductsController {
       }
     } catch (error) {
       console.log(error);
-      return res.json({ msg: "An error has ocurred" });
+      return res.status(500).json({ msg: error.message });
     }
   }
 
   // create a new product
 
   public async createProduct(req: Request, res: Response): Promise<Response> {
-    // const id = uuidv4();
     const id = uuidv4();
     const product: Product = {
       ...req.body,
-      id, 
+      id,
       status: true,
     };
-    const products = await dataFile.readProductsFile();
-    products.push(product);
-    dataFile.writeProductsFile(products);
-    return res.json({ msg: "Product successfully created" });
+    try {
+      const products = await dataFile.readProductsFile();
+      products.push(product);
+      dataFile.writeProductsFile(products);
+      return res.json({ msg: "Product successfully created" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
   }
 
   // update a product
@@ -63,38 +66,46 @@ class ProductsController {
   public async updateProduct(req: Request, res: Response): Promise<Response> {
     if (!req.params.pid)
       return res.status(400).json({ msg: "Bad request: missing product ID" });
+    try {
+      const products = await dataFile.readProductsFile();
+      const index = products.findIndex((p) => p.id === req.params.pid);
+      if (index < 0) {
+        return res.status(404).json({ msg: "No product matches that ID" });
+      } else {
+        if (req.body.title) products[index].title = req.body.title;
+        if (req.body.description)
+          products[index].description = req.body.description;
+        if (req.body.code) products[index].code = req.body.code;
+        if (req.body.price) products[index].price = req.body.price;
+        if (req.body.status) products[index].status = req.body.status;
+        if (req.body.stock) products[index].stock = req.body.stock;
+        if (req.body.category) products[index].category = req.body.category;
+        if (req.body.thumbnails)
+          products[index].thumbnails = req.body.thumbnails;
 
-    const products = await dataFile.readProductsFile();
-    const index = products.findIndex((p) => p.id === req.params.pid);
-    if (index < 0) {
-      return res.status(404).json({ msg: "No product matches that ID" });
-    } else {
-      if (req.body.title) products[index].title = req.body.title;
-      if (req.body.description)
-        products[index].description = req.body.description;
-      if (req.body.code) products[index].code = req.body.code;
-      if (req.body.price) products[index].price = req.body.price;
-      if (req.body.status) products[index].status = req.body.status;
-      if (req.body.stock) products[index].stock = req.body.stock;
-      if (req.body.category) products[index].category = req.body.category;
-      if (req.body.thumbnails) products[index].thumbnails = req.body.thumbnails;
-
-      dataFile.writeProductsFile(products);
-      return res.status(200).json({ msg: "Product successfully updated" });
+        dataFile.writeProductsFile(products);
+        return res.status(200).json({ msg: "Product successfully updated" });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
     }
   }
 
   // delete a product
 
   public async deleteProduct(req: Request, res: Response): Promise<Response> {
-    const products = await dataFile.readProductsFile();
-    const product = products.find((p) => p.id === req.params.pid);
-    if (product) {
-      const newProducts = products.filter((p) => p.id != req.params.pid);
-      await dataFile.writeProductsFile(newProducts);
-      return res.status(200).json({ msg: "Product successfully deleted" });
-    } else {
-      return res.status(404).json({ msg: "No product matches that ID" });
+    try {
+      const products = await dataFile.readProductsFile();
+      const product = products.find((p) => p.id === req.params.pid);
+      if (product) {
+        const newProducts = products.filter((p) => p.id != req.params.pid);
+        await dataFile.writeProductsFile(newProducts);
+        return res.status(200).json({ msg: "Product successfully deleted" });
+      } else {
+        return res.status(404).json({ msg: "No product matches that ID" });
+      }
+    } catch (error) {
+      return res.status(500).json({msg: error.message})
     }
   }
 }
