a50ac3ad79b8cd3447cf56256752a82b1e32ec01
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-21 11:44:20 -0500

    add createCart function

--- src/controllers/carts.controller.ts
+++ src/controllers/carts.controller.ts
@@ -2,12 +2,25 @@ import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 import Cart from "../types/cart";
 import { dataFile } from "../helpers/data.helper";
+import { json } from "stream/consumers";
 
-class CartsController{
-    public async createCart(req: Request, res:Response){
-
+class CartsController {
+  public async createCart(req: Request, res: Response): Promise<Response> {
+    const id = uuidv4();
+    const cart: Cart = {
+      id,
+      ...req.body,
+    };
+    try {
+        const carts = await dataFile.readCartsFile();
+        carts.push(cart);
+        await dataFile.writeCartsFile(carts);
+        return res.send({"msg": "cart succcessfully created"})
+    } catch (error) {
+        return res.json({"error": error.message})
     }
+  }
 }
 
-const cartsController = new CartsController
-export {cartsController}
+const cartsController = new CartsController();
+export { cartsController };
