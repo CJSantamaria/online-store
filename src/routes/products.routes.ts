e0e5424d80e2158b3dc348a8c75c954b409049ca
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-22 13:35:05 -0500

    add normalizeFieldNames middleware to the products update route

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import { productsController as pc } from "../controllers/products.controller";
-import { validateCreate } from "../validators/products.validator";
-
+import {
+  validateCreate,
+  validateUpdate,
+} from "../validators/products.validator";
+import {
+  trimStringValues,
+  normalizeFieldNames,
+} from "../middlewares/strings.midddleware";
 
 class ProductRoutes {
   public router: Router;
@@ -14,7 +20,13 @@ class ProductRoutes {
     this.router.get("/:pid", pc.getProduct);
     this.router.post("/", validateCreate, pc.createProduct);
     this.router.put("/", pc.updateProduct);
-    this.router.put("/:pid", pc.updateProduct);
+    this.router.put(
+      "/:pid",
+      trimStringValues,
+      normalizeFieldNames,
+      validateUpdate,
+      pc.updateProduct
+    );
     this.router.delete("/:pid", pc.deleteProduct);
   }
 }
