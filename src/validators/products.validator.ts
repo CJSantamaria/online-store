0b84ec9c763f83715d2fda685c6269b3db402dc1
Carlos Santamaria <carlosjose.santamaria@endava.com>
2023-12-22 08:33:22 -0500

    tweak the product creation validation

--- src/validators/products.validator.ts
+++ src/validators/products.validator.ts
@@ -3,15 +3,30 @@ import { check } from "express-validator";
 import { validateResult } from "../helpers/validate.helper";
 
 const validateCreate = [
-  check("id").not().exists(),
-  check("title").exists().isLength({ min: 3 }),
-  check("description").exists().isLength({ min: 10 }),
-  check("code").exists().not().isEmpty(),
+  check("id")
+    .not()
+    .exists()
+    .withMessage(
+      "Id is automatically generated, omit field in the request body"
+    ),
+  check("title")
+    .exists()
+    .isLength({ min: 3 })
+    .withMessage("title is required and must be at least 3 characters long"),
+  check("description")
+    .exists()
+    .isLength({ min: 10 })
+    .withMessage("description is required and must be at least 10 characters long"),
+  check("code")
+    .exists()
+    .not()
+    .isEmpty()
+    .withMessage("code is required and cannot be empty"),
   check("category").exists().isLength({ min: 3 }),
   check("status").custom((value, { req }) => {
     if (value !== undefined && value !== true) {
       throw new Error(
-        "status field is true by default, you can ommit it in your request body"
+        "product status is true by default, omit field in the request body"
       );
     }
     return true;
@@ -20,8 +35,8 @@ const validateCreate = [
     .exists()
     .isNumeric()
     .custom((value, { req }) => {
-      if (value < 1) {
-        throw new Error("Price must be at least $1");
+      if (value <= 0) {
+        throw new Error("price must be a positive number");
       }
       return true;
     }),
@@ -33,7 +48,7 @@ const validateCreate = [
     .isNumeric()
     .custom((value) => {
       if (value < 0) {
-        throw new Error("Stock must be a positive number");
+        throw new Error("stock must be a positive number");
       }
       return true;
     }),
