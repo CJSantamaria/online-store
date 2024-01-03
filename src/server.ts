a8dfb8b025c9ccebc040e11dfcb0b613d5d3b15b
Carlos Santamaria <carlosjose.santamaria@endava.com>
2024-01-03 11:15:07 -0500

    add the database connection string

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,34 @@
 import express from "express";
+import mongoose from "mongoose";
 import morgan from "morgan";
 import productsRoutes from "./routes/products.routes";
 import cartsRoutes from "./routes/carts.routes";
+import dotenv from "dotenv";
 
 class Server {
   public app: express.Application;
   constructor() {
     this.app = express();
     this.config();
+    this.connectDB();
     this.routes();
   }
 
   config() {
+    dotenv.config();
     this.app.set("port", process.env.PORT || 8080);
     this.app.use(express.json());
     this.app.use(morgan("dev"));
+  }
+  connectDB() {
+    mongoose
+      .connect(process.env.MONGODB_URI!)
+      .then(() => {
+        console.log("database connected");
+      })
+      .catch((error) => {
+        console.log("Couldn't reach the database:", error);
+      });
   }
   routes() {
     this.app.use("/api/products", productsRoutes);
