b478aa89748adc1c676ab303b5073ffed0708216
Carlos Santamaria <carlosjose.santamaria@endava.com>
2024-01-17 12:33:39 -0500

    add a definition for the cart schema and model

---
+++ src/models/cart.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface Product {
+  productId: mongoose.Schema.Types.ObjectId;
+  quantity: number;
+}
+
+interface Cart extends Document {
+  products: Product[];
+}
+
+const cartSchema = new Schema<Cart>({
+  products: [
+    {
+      _id:false,
+      productId: { type: Schema.Types.ObjectId, required: true },
+      quantity: { type: Number, required: true },
+    },
+  ],
+});
+
+const CartModel = mongoose.model<Cart>('Cart', cartSchema);
+
+export default CartModel;
+
